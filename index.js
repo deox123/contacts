f07c8d8ca9d770cc5f76585c9dac80f7e74b9d7d
deox123 <nikolamilosevic93@yahoo.com>
2018-12-02 20:21:23 +0100

    resolved cors issues

--- index.js
+++ index.js
@@ -14,6 +14,25 @@ db.once('open', function() {
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 
 const personSchema = new mongoose.Schema({
     name: String,
@@ -32,7 +51,7 @@ app.get('/persons', (req, res) => {
         if (err) return console.log(err)
         res.send(JSON.stringify(persons));
     })
-});;
+});
 
 app.post('/new', (req, res) => {
     const newPerson = new Person({
