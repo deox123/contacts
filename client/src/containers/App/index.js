bee6acaa5aa5fef4257d27da109a2a5fe6452218
deox123 <nikolamilosevic93@yahoo.com>
2018-12-02 20:17:31 +0100

    Implemented router

--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import Home from '../../routes/HomeRoute/index';
+import Detail from '../../components/Detail';
 
 const App = () => (
-    <Home />
+    <Router>
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                </ul>
+            </nav>
+
+            <Route path="/" exact component={Home} />
+            <Route path="/:id" render={props => <Detail {...props} extra={{asd:123 }} />} />
+        </div>
+    </Router>
 );
 
 export default App;
