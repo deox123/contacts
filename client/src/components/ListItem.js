a28923cdd6f9103de955d4834d68c5eb1dc11457
deox123 <nikolamilosevic93@yahoo.com>
2018-12-06 00:11:19 +0100

    implemented listItem component

--- client/src/components/ListItem.js
+++ client/src/components/ListItem.js
@@ -1,13 +1,43 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
+import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined';
+import Button from '@material-ui/core/Button';
+import Tooltip from '@material-ui/core/Tooltip';
+import './listItem.css';
+
+import { Route } from 'react-router-dom'
+
 
 const ListItem = (props) => {
     const {
         person,
-    } = props
-    return (    
-            <Link to={`/${person._id}`} >{person.name}</Link>
+        handleDelete,
+    } = props;
+
+    const navigate = (history, id) => () => history.push(`/${id}`)
+
+    return (
+        <Route render={({history}) => (
+            
+            <React.Fragment>
+                <TableRow>
+                    <TableCell>
+                        <Tooltip title="Show more" placement="right">
+                            <span onClick={navigate(history, person._id)}>{person.name}</span>
+                        </Tooltip>
+                    </TableCell>
+                    <TableCell>{person.surname}</TableCell>
+                    <TableCell>{person.phone}</TableCell>
+                    <TableCell>
+                        
+                        <DeleteOutlinedIcon className="delete-icon" onClick={handleDelete(person._id)}/>
+                    </TableCell>
+                </TableRow>
+            </React.Fragment>
+        )} />
         )
 }
 
