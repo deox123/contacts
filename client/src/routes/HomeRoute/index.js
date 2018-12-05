498fe4d05bd0283047b060756066453c103971c6
deox123 <nikolamilosevic93@yahoo.com>
2018-12-06 00:12:05 +0100

    home componenet view added

--- client/src/routes/HomeRoute/index.js
+++ client/src/routes/HomeRoute/index.js
@@ -3,6 +3,18 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableCell from '@material-ui/core/TableCell';
+import Paper from '@material-ui/core/Paper';
+import TableRow from '@material-ui/core/TableRow';
+import TableBody from '@material-ui/core/TableBody';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+
+
+import './styles.css';
+
 
 import {Link} from "react-router-dom";
 
@@ -35,14 +47,40 @@ export class Home extends Component<Props, State> {
         this.props.actions.getPersons();
     }
 
+    handleDelete = (id: number) => () => {
+        console.log(id)
+    }
+
+    navigateToDetail = (id: number) => () => {
+        console.log(id);        
+    }
+
     render() {
         return (
-            <div> 
-                <div>Contacts</div>
-                <ul>
-                    {this.props.listOfContacts.map(person => <ListItem key={person._id} person={person}/>)}
-                </ul>
-            </div>
+            <Paper className="container">
+                <h1>Contacts</h1>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Name</TableCell>
+                            <TableCell>Surname</TableCell>
+                            {/* <TableCell>Created date</TableCell>
+                            <TableCell>City</TableCell>
+                            <TableCell>Address</TableCell> */}
+                            <TableCell>phone</TableCell>  
+                            <TableCell></TableCell>
+                        </TableRow>                    
+                    </TableHead>
+                    <TableBody>
+                        {this.props.listOfContacts.map(person => <ListItem key={person._id} person={person} handleShowMore={this.navigateToDetail} handleDelete={this.handleDelete} />)}
+                    </TableBody>
+                </Table>
+                <Link to="/new">
+                <Fab color="primary" aria-label="Add" className="fab">
+                    <AddIcon />
+                </Fab>
+                </Link>
+            </Paper>
         );
     }
 }
